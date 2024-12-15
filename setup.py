343013448ca7efc32322a7518da4ea0fef8c2503
kenyo3023 <kenyo3023@gmail.com>
2024-12-15 15:27:17 +0800

    [FEAT] Enable pip install through git+https

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="openai-wrapper",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "openai>=1.0.0",
+    ],
+    author="kenyo3023",
+    author_email="kenyo3023@gmail.com",
+    description="A wrapper for OpenAI API with default parameter support",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenyo3023/OpenAI-Compatible-Wrapper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
